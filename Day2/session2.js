8e7fb2c35100b088c688b43cdfbfcb1cec89896d
subham-kr-ds <subhamdas1508@gmail.com>
2023-05-31 08:48:29 +0530

    htmlcssjs

---
+++ Day2/session2.js
@@ -0,0 +1,70 @@
+const button1 = document.getElementById("clickable1");
+//const button2 = document.getElementById("clickable2");
+//const textarea = document.getElementById("content");
+
+/*button1.addEventListener("click",()=>{
+    textarea.innerText="This is My Text.";
+});
+
+button2.addEventListener("click",()=>{
+    textarea.innerText="";
+});*/
+
+/*const table= document.querySelector("tbody");
+
+button1.addEventListener("click",()=>{
+    const tr= document.createElement("tr");
+    const td1= document.createElement("td");
+    td1.innerText="1";
+    const td2= document.createElement("td");
+    td2.innerText="Subham";
+    const td3= document.createElement("td");
+    td3.innerText="Male";
+    tr.append(td1,td2,td3);
+    table.append(tr);
+});
+
+button2.addEventListener("click",()=>{
+    window.location.reload();
+});*/
+/*const button1 = document.getElementById("clickable1");
+//const button2 = document.getElementById("clickable2");
+//const textarea = document.getElementById("content");
+
+/*button1.addEventListener("click",()=>{
+    textarea.innerText="This is My Text.";
+});
+
+button2.addEventListener("click",()=>{
+    textarea.innerText="";
+});*/
+
+/*const table= document.querySelector("tbody");
+
+button1.addEventListener("click",()=>{
+    const tr= document.createElement("tr");
+    const td1= document.createElement("td");
+    td1.innerText="1";
+    const td2= document.createElement("td");
+    td2.innerText="Subham";
+    td2.style.color="red";
+    const td3= document.createElement("td");
+    td3.innerText="Male";
+    tr.append(td1,td2,td3);
+    table.append(tr);
+});
+
+button2.addEventListener("click",()=>{
+    window.location.reload();
+});*/
+/*button1.addEventListener("click",()=>{
+    const div = document.getElementById("mydiv")
+    const image=document.createElement("img");
+    image.src="vanila.png";
+    image.style.height="200px";
+    div.append(image);
+    
+})*/
+ button1.addEventListener("click",()=>{
+    const emptydiv = document.getElementById("toshow");
+ })
